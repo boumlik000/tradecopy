13af9e56e8921fdef209ba6e74bc36bfd96da308
mboumlik <mboumlik@kali>
2025-12-07 14:19:27 +0100

    www

--- app/page.tsx
+++ app/page.tsx
@@ -77,7 +77,7 @@ export default function SampherosisPage() {
         setCopiedId(null)
         setShowToast(false)
       }, 1500)
-    } catch () {
+    } catch (_err) {
       console.error("Failed to copy text")
     }
   }
